e728fc2c13ac95fe7691bc1b21f0148b36b0173c
Dana Obaid <danaobaid97@gmail.com>
2026-09-26 00:18:02 +0200

    Complete User features By Dana

--- library_app/models/user.py
+++ library_app/models/user.py
@@ -9,28 +9,40 @@ class User:
     """Represents a library user."""
 
     def __init__(self, name, password):
-        pass
+        self.name = name
+        self.password = password
+        self.borrowed_books = []  # list of book title
 
     def borrow_book(self, book):
         """Add a book to the user's borrowed list."""
-        pass
+        self.borrowed_books.append(book.title)
 
     def return_book(self, book):
         """Remove a book from the user's borrowed list."""
-        pass
+        if book.title in self.borrowed_books:
+            self.borrowed_books.remove(book.title)
+            return True
+        return False
+
 
     def list_borrowed_books(self):
         """Return a list of borrowed book titles."""
-        pass
+        return self.borrowed_books
 
     def to_dict(self):
         """Convert user to a dictionary for JSON serialization."""
-        pass
+        return {
+            "name": self.name,
+            "password": self.password,
+            "borrowed_books": self.borrowed_books,
+        }
 
     @classmethod
     def from_dict(cls, data):
         """Create a User instance from a dictionary."""
-        pass
+        user = cls(data["name"], data["password"])
+        user.borrowed_books = data.get("borrowed_books", [])
+        return user
 
     def __repr__(self):
-        pass
+        return f"User(name={self.name!r}, borrowed={len(self.borrowed_books)})"
